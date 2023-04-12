b247dcae67da9d330977bf409f6101705bfde77b
BabiychukYuliya <julia4233097@gmail.com>
2023-04-12 13:01:05 +0300

    task_02

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Screens/LoginScreen';
+import RegistrationScreen from './Screens/RegistrationScreen';
+import { useState, useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js!!!</Text>
+  const [statusLog, setStatusLog] = useState(false);
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('../myNewProject/assets/font/Roboto-Regular.ttf'),
+  });
 
-            <StatusBar style="auto" />
-    </View>
-  );
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    // <RegistrationScreen></RegistrationScreen>
+    <LoginScreen></LoginScreen>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
